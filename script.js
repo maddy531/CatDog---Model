f770ad9cf3eb1c94329320fe0b36e60ea53118f2
Maddy <maddy531@gmail.com>
2015-11-19 15:08:13 -0500

    Added two more tabs, created cutsome error link.

--- script.js
+++ script.js
@@ -11,6 +11,16 @@ app.config(function($routeProvider){
     controller: 'routeCtrl'
   });
 
+  $routeProvider.when('/view3', {
+    templateUrl: 'view3.html',
+    controller: 'routeCtrl'
+  });
+
+  $routeProvider.when('/view4', {
+    templateUrl: 'view4.html',
+    controller: 'routeCtrl'
+  });
+
   $routeProvider.otherwise({
   templateUrl: '404error.html',
   controller: 'routeCtrl'
